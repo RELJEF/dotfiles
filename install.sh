cdb95c127a09c35ca73b13b8f958f3864ae8bd3e
RELJEF <reljef2@gmail.com>
2018-03-28 21:22:26 +0200

    Added VIM config and script to install dotfiles.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+INSTALL_DIR=${1:-$HOME}
+
+exit_msg=0
+
+echo -n "Install VIM configuration? [N/y] "
+read -n 1 vim_cfg
+echo
+
+if [ "$vim_cfg" == "Y" ] || [ "$vim_cfg" == "y" ]; then
+    mv $INSTALL_DIR/.vim $INSTALL_DIR/.vim.old 2> /dev/null
+    mv $INSTALL_DIR/.vimrc $INSTALL_DIR/.vimrc.old 2> /dev/null
+    ln -s $PWD/.vim $INSTALL_DIR/.vim 2> /dev/null
+    ln -s $PWD/.vimrc $INSTALL_DIR/.vimrc 2> /dev/null
+    exit_msg=1
+fi
+
+if [ "$exit_msg" == 0 ]; then
+    echo "No configuration files were installed."
+else
+    echo "Done. Existing configuration files were moved to <file>.old"
+fi
+
